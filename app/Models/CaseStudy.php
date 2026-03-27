68238f3e4413c27608eca163c726e6715011497b
acasekc <50672243+acasekc@users.noreply.github.com>
2026-03-26 23:38:14 -0500

    Add hero_image accessor for cloud URL resolution

--- app/Models/CaseStudy.php
+++ app/Models/CaseStudy.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class CaseStudy extends Model
 {
@@ -31,4 +32,39 @@ class CaseStudy extends Model
         'metrics' => 'array',
         'is_published' => 'boolean',
     ];
+
+    public function getHeroImageAttribute(?string $value): ?string
+    {
+        if (! is_string($value) || $value === '') {
+            return $value;
+        }
+
+        if (str_starts_with($value, 'http://') || str_starts_with($value, 'https://')) {
+            return $value;
+        }
+
+        if (! str_starts_with($value, '/storage/')) {
+            return $value;
+        }
+
+        $assetDisk = $this->getPublicAssetDisk();
+
+        if ($assetDisk === 'public') {
+            return $value;
+        }
+
+        $relativePath = ltrim(str_replace('/storage/', '', $value), '/');
+
+        /** @var \Illuminate\Filesystem\FilesystemAdapter $storage */
+        $storage = Storage::disk($assetDisk);
+
+        return $storage->url($relativePath);
+    }
+
+    private function getPublicAssetDisk(): string
+    {
+        $defaultDisk = (string) config('filesystems.default', 'local');
+
+        return $defaultDisk === 'local' ? 'public' : $defaultDisk;
+    }
 }
